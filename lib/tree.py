3523502d60fc35be5b157ec8ae4f628c5c043ae1
Jasec <jasec_7@hotmail.com>
2025-09-01 22:01:18 -0700

    complete python p3 dsa tree traversal

--- lib/tree.py
+++ lib/tree.py
@@ -2,5 +2,20 @@ class Tree:
   def __init__(self, root = None):
     self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+  def get_element_by_id(self, target_id):
+    nodes_to_visit = [self.root]
+
+    if self.root is None:
+      return None
+    
+    while len(nodes_to_visit) > 0:
+      node = nodes_to_visit.pop()
+      if node['id'] == target_id:
+        return node
+      
+      nodes_to_visit.extend(node['children'])
+    return None
+      
+        
+  
+    
